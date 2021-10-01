121308ba1b69e572417ee3c4f3fe756b2a31ec38
t-shinkai00 <t.shinkai00@gmail.com>
2021-10-01 15:09:59 +0900

    lower variables

--- FaceApi.py
+++ FaceApi.py
@@ -4,9 +4,10 @@ import json
 
 with open("secret.json") as f:
     secret_json=json.load(f)
-SUBSCRIPTION_KEY=secret_json["SUBSCRIPTION_KEY"]
-ENDPOINT=secret_json["ENDPOINT"]
-# print(SUBSCRIPTION_KEY)
-assert SUBSCRIPTION_KEY
-face_api_url=ENDPOINT+"face/v1.0/detect"
+subscription_key=secret_json["SUBSCRIPTION_KEY"]
+endpoint=secret_json["ENDPOINT"]
+
+# print(subscription_key)
+assert subscription_key
+face_api_url=endpoint+"face/v1.0/detect"
 # print(face_api_url)
