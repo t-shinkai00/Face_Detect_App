09a1d79e966c6a954fe77f342432c9c2e2baff84
t-shinkai00 <t.shinkai00@gmail.com>
2021-10-01 15:07:50 +0900

    import endpoint

--- FaceApi.py
+++ FaceApi.py
@@ -5,4 +5,8 @@ import json
 with open("secret.json") as f:
     secret_json=json.load(f)
 SUBSCRIPTION_KEY=secret_json["SUBSCRIPTION_KEY"]
-print(SUBSCRIPTION_KEY)
+ENDPOINT=secret_json["ENDPOINT"]
+# print(SUBSCRIPTION_KEY)
+assert SUBSCRIPTION_KEY
+face_api_url=ENDPOINT+"face/v1.0/detect"
+# print(face_api_url)
