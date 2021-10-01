807507c28e8fed792d87768ca3b0562b7d917297
t-shinkai00 <t.shinkai00@gmail.com>
2021-10-01 13:43:18 +0900

    practice

--- main.py
+++ main.py
@@ -3,3 +3,24 @@ import pandas as pd
 import numpy as np
 
 st.title("My 1st App")
+
+st.write("データフレーム")
+st.write(
+  pd.DataFrame({
+    "1st column":[1,2,3,4],
+    "2nd column":[10,20,30,40],
+  })
+)
+
+"""
+# My 1st App
+## マジックコマンド
+Markdown対応
+"""
+
+if st.checkbox("Show DataFrame"):
+  chart_df=pd.DataFrame(
+    np.random.randn(20,3),
+    columns=["a","b","c"]
+  )
+  st.line_chart(chart_df)
