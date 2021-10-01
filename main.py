0f376c85caf236a0ec6eb0e725412cb835fb5bdb
t-shinkai00 <t.shinkai00@gmail.com>
2021-10-01 14:00:39 +0900

    add upload function

--- main.py
+++ main.py
@@ -1,26 +1,10 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
+from PIL import Image
 
-st.title("My 1st App")
+st.title("顔認識アプリ")
 
-st.write("データフレーム")
-st.write(
-  pd.DataFrame({
-    "1st column":[1,2,3,4],
-    "2nd column":[10,20,30,40],
-  })
-)
+uploaded_file=st.file_uploader("Choose an image...", type="jpg")
 
-"""
-# My 1st App
-## マジックコマンド
-Markdown対応
-"""
-
-if st.checkbox("Show DataFrame"):
-  chart_df=pd.DataFrame(
-    np.random.randn(20,3),
-    columns=["a","b","c"]
-  )
-  st.line_chart(chart_df)
+if uploaded_file is not None:
+  img=Image.open(uploaded_file)
+  st.image(img, caption="Uploaded Image.", use_column_width=True)
