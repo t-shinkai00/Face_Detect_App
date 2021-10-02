c04e2059644a49d9747560f300e1793ca7c41f3f
t-shinkai00 <t.shinkai00@gmail.com>
2021-10-03 03:37:53 +0900

    display Japanese only

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import streamlit as st
 from PIL import Image
 from FaceApi import detect_face
-st.title("顔検出アプリ")
+st.title("Face Detector")
 
-uploaded_file=st.file_uploader("画像を選択", type="jpg")
+uploaded_file=st.file_uploader("Choose a jpg image", type="jpg")
 
 if uploaded_file is not None:
   img=Image.open(uploaded_file)
   # print(uploaded_file,img)
   detect_face(img)
-  st.image(img, caption="検出結果", use_column_width=True)
+  st.image(img, caption="Result", use_column_width=True)
