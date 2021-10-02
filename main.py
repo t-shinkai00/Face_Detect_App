c4b088dbf70014d7ca50b3e5636c4af3ce0abb1b
t-shinkai00 <t.shinkai00@gmail.com>
2021-10-02 11:32:31 +0900

    import FaceApi

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import streamlit as st
 from PIL import Image
-from Face_API import detect_face
-
+from FaceApi import detect_face
 st.title("顔認識アプリ")
 
 uploaded_file=st.file_uploader("Choose an image...", type="jpg")
 
 if uploaded_file is not None:
   img=Image.open(uploaded_file)
+  # print(uploaded_file,img)
+  detect_face(img)
   st.image(img, caption="Uploaded Image.", use_column_width=True)
