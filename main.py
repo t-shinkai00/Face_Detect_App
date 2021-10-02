ce6a8dd706e280deb3d7bead7465f752c046f94c
t-shinkai00 <t.shinkai00@gmail.com>
2021-10-03 03:33:04 +0900

    display Japanese only

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from PIL import Image
 from FaceApi import detect_face
 st.title("顔検出アプリ")
 
-uploaded_file=st.file_uploader("Choose an image...", type="jpg")
+uploaded_file=st.file_uploader("画像を選択", type="jpg")
 
 if uploaded_file is not None:
   img=Image.open(uploaded_file)
   # print(uploaded_file,img)
   detect_face(img)
-  st.image(img, caption="Uploaded Image.", use_column_width=True)
+  st.image(img, caption="検出結果", use_column_width=True)
